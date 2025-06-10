1c35fce6b8801afbac506990e29b4dad23ecbbde
kkkkimtaehyeon <kth1927@naver.com>
2025-06-11 01:23:17 +0900

    redis 설정 수정

--- config/redis_config.py
+++ config/redis_config.py
@@ -5,10 +5,10 @@ import redis.asyncio as redis
 
 _ = load_dotenv(find_dotenv())
 
-HOST = os.getenv("redis_host")
-PORT = os.getenv("redis_port")
-DB = os.getenv("redis_db_ai")
-USERNAME = os.getenv("redis_username")
-PASSWORD = os.getenv("redis_password")
+HOST = os.getenv("redis-host")
+PORT = int(os.getenv("redis-port"))
+DB = os.getenv("redis-db-ai")
+USERNAME = os.getenv("redis-username")
+PASSWORD = os.getenv("redis-password")
 
 redis_client = redis.Redis(host=HOST, port=PORT, username=USERNAME, db=DB, password=PASSWORD)
