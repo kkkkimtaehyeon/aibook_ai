271bd2d6fff6e9cf440267be22e722c7e2e73489
kkkkimtaehyeon <kth1927@naver.com>
2025-06-11 01:22:56 +0900

    cors 허용

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174", "http://localhost:8080"],  # 프론트 서버, API 서버
+    allow_origins=["*"],  # 프론트 서버, API 서버
+    # allow_origins=["https://aibook-front.netlify.app", "http://localhost:5174", "http://localhost:8080"],  # 프론트 서버, API 서버
     allow_credentials=True,
-    allow_methods=["POST"],  # 모든 메서드 허용
+    allow_methods=["POST", "GET", "PUT", "DELETE"],  # 모든 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
 )
 
