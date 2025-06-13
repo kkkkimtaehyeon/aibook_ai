09d11c0fedaecef48a7a03aebfb85e2d0f5ac90c
kkkkimtaehyeon <kth1927@naver.com>
2025-06-13 22:10:12 +0900

    로그레벨 debug -> info 수정

--- utils/logConfig.py
+++ utils/logConfig.py
@@ -2,7 +2,7 @@ import logging
 
 
 logging.basicConfig(
-    level=logging.DEBUG,  # 디버그 레벨까지 출력되도록 설정
+    level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
         logging.StreamHandler(),
@@ -10,4 +10,4 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger("aibook")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
